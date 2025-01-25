e9315696933fa4e48baf57cd052e7dc1a32ec7f7
Fernando Fernandez-Cormenzana Luna <fernansan.xd@gmail.com>
2025-01-25 18:07:40 +0100

    Push

--- Assets/script/EndGame.cs
+++ Assets/script/EndGame.cs
@@ -4,25 +4,25 @@ using System.Collections;
 
 public class EndGame : MonoBehaviour
 {
-    public Image imagenAMostrar; // Referencia a la imagen que quieres mostrar
-    //public Aguantar1 scriptCount; // Referencia al script Count
-    public Aguantar scriptCount; // Referencia al script Count
+    //public Image imagenAMostrar; // Referencia a la imagen que quieres mostrar
+    ////public Aguantar1 scriptCount; // Referencia al script Count
+    //public Aguantar scriptCount; // Referencia al script Count
 
-    public float tiempoTransicion = 1f; // Tiempo en segundos que dura la transición
-    private float tiempoTranscurrido = 0f;
+    //public float tiempoTransicion = 1f; // Tiempo en segundos que dura la transición
+    //private float tiempoTranscurrido = 0f;
 
-    private void Update()
-    {
-        // Verificar si el shake timer en el script Count ha llegado a 1
-        if (scriptCount.shakeTimer <= 1)
-        {
-            // Si la imagen no es nula, iniciar la transición
-            if (imagenAMostrar != null)
-            {
-                tiempoTranscurrido += Time.deltaTime;
-                float t = Mathf.Clamp01(tiempoTranscurrido / tiempoTransicion); // Valor entre 0 y 1 para la interpolación
-                imagenAMostrar.color = Color.Lerp(Color.clear, Color.black, t);
-            }
-        }
-    }
+    //private void Update()
+    //{
+    //    // Verificar si el shake timer en el script Count ha llegado a 1
+    //    if (scriptCount.shakeTimer <= 1)
+    //    {
+    //        // Si la imagen no es nula, iniciar la transición
+    //        if (imagenAMostrar != null)
+    //        {
+    //            tiempoTranscurrido += Time.deltaTime;
+    //            float t = Mathf.Clamp01(tiempoTranscurrido / tiempoTransicion); // Valor entre 0 y 1 para la interpolación
+    //            imagenAMostrar.color = Color.Lerp(Color.clear, Color.black, t);
+    //        }
+    //    }
+    //}
 }
