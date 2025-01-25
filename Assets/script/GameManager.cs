10bf0095d0668eac06dcc7bd03e66b2c7f512110
Fernando Fernandez-Cormenzana Luna <fernansan.xd@gmail.com>
2025-01-25 17:52:54 +0100

    ProyectoActualizado

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,7 +1,16 @@
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    public int BombonasARecoger;
+    public TextMeshProUGUI textoBombonasMax;
+
+    private void OnEnable()
+    {
+        textoBombonasMax.SetText("");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
